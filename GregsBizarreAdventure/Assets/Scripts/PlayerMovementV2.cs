0dc4eca11ceed8e85272fa41aa8b05afbfbbb18d
HakimAmraoui <hakim.amraoui@outlook.fr>
2020-10-24 13:42:10 +0300

    Jump Animation Fixed
    Fixed the jump animation and redid properly the script

---
+++ GregsBizarreAdventure/Assets/Scripts/PlayerMovementV2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementV2 : MonoBehaviour
+{
+    [SerializeField] private CharacterController2D controller;
+    [SerializeField] private Animator animator;
+
+    private float horizontalMove = 0f, runSpeed = 40;
+    private bool jump = false, crounch = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crounch, jump);
+        jump = false;
+    }
+}
